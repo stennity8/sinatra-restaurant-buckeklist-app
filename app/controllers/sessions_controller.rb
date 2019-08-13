91063ad747e33de4d61e46af49b37bf83c00d136
Shawn <shawntennity@gmail.com>
2019-08-13 15:11:10 -0400

    Created SessionController to handle login/logout.  Moved register to UserController.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+class SessionsController < ApplicationController
+
+  get '/login' do
+    if is_logged_in?(session)
+      flash[:message] = "You are already logged in."
+      redirect '/reviews'
+    end
+      erb :"/sessions/new"
+  end
+
+  post '/login' do
+    # binding.pry
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+
+      redirect "/reviews"
+    else
+      flash[:message] = "Incorrect login credentials.  Please try again."
+      redirect "/login"
+    end
+  end
+
+  delete '/logout' do
+    if is_logged_in?(session)
+      session.destroy
+      redirect "/login"
+    else
+      redirect "/"
+    end
+   end
+
+end
