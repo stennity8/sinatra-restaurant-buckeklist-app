9ed9a95fb12446d42a41005a075fe01c1754633a
Shawn <shawntennity@gmail.com>
2019-08-14 12:04:36 -0400

    Formated registration page and updated login page formatting.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   include Memorable::InstanceMethods
 
   validates_presence_of :username, :email, :password_digest
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
   has_secure_password
   has_many :reviews
   has_many :restaurants, through: :reviews
