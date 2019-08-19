717c1b8530e13ced9047dc3eb4b5c51ffd3195c6
Shawn <shawntennity@gmail.com>
2019-08-19 17:13:09 -0400

    Updated configuration for developement for Heroku deployment.

--- Rakefile
+++ Rakefile
@@ -3,14 +3,6 @@ ENV["SINATRA_ENV"] ||= "development"
 require_relative './config/environment'
 require 'sinatra/activerecord/rake'
 
-desc 'Clear db to start fresh while testing'
-task :remigrate do
-  Rake::Task["db:rollback"].execute
-  Rake::Task["db:rollback"].execute
-  Rake::Task["db:rollback"].execute
-  Rake::Task["db:migrate"].execute 
-end
-
 desc 'A Pry console'
 task :console do
   Pry.start
--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,15 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+
+configure :development do
+  ENV['SINATRA_ENV'] ||= "development"
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
+end
 
 configure :production do
 	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
