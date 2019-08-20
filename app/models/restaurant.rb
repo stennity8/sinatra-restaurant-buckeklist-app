e16c0ac7419a3a48b08f1e4201d06dbc56d15e04
Shawn <shawntennity@gmail.com>
2019-08-20 12:37:42 -0400

    Removed module inheritance from restaurant class since it was not being used.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,4 @@
 class Restaurant < ActiveRecord::Base
-  extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
-
   validates_presence_of :name, :cuisine, :address, :city, :state, :zip_code
   has_many :reviews
   has_many :users, through: :reviews
